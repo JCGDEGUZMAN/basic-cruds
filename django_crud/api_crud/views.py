7e0cfe32b8713e181a0186d33242e15eb1a42bfe
JCGDEGUZMAN <julius.d@cloudwalkdigital.com>
2020-08-26 22:27:01 +0800

    GenericViewSet

--- django_crud/api_crud/views.py
+++ django_crud/api_crud/views.py
@@ -9,41 +9,12 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
-class SubjectViewset(viewsets.ViewSet):
+class SubjectViewset(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.RetrieveModelMixin, mixins.DestroyModelMixin):
     
-    def list(self, request):
-        subjects = Subject.objects.all()
-        serializer = SubjectSerializer(subjects, many=True)
-        return Response(serializer.data)
-
-    def create(self, request):
-        serializer = SubjectSerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
-
-    def retrieve(self, request, pk=None):
-         queryset = Subject.objects.all()
-         subject = get_object_or_404(queryset, pk=pk)
-         serializer = SubjectSerializer(subject)
-         return Response(serializer.data)
-    
-    def update(self, request, pk=None):
-        subject = Subject.objects.get(pk=pk)
-        serializer = SubjectSerializer(subject, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk=None):
-            subject = Subject.objects.get(pk=pk)
-            subject.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
+    serializer_class = SubjectSerializer
+    queryset = Subject.objects.all()
